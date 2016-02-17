46cbaaee85f7da7a2835c420d2d0c0ae1a229c3d
Charles Lehner <cel@celehner.com>
2016-02-16 21:38:49 -0500

    Add git-ssb command

--- bin.js
+++ bin.js
@@ -1,41 +1,44 @@
 #!/usr/bin/env node
-
-var ref = require('ssb-ref')
-var path = require('path')
-var toPull = require('stream-to-pull-stream')
-var pull = require('pull-stream')
+var spawn = require('child_process').spawn
 var ssbGit = require('ssb-git')
+var pull = require('pull-stream')
 
 var args = process.argv.slice(2)
-if (args.length < 2)
-  throw new Error('Not enough arguments')
+switch (args[0]) {
+  case 'create':
+    createRepo(args[1])
+    break
 
-var alias = args[0]
-var url = args[1]
+  case undefined:
+  case '-h':
+    console.log([
+      'Usage: git ssb [command]',
+      '',
+      'Commands:',
+      '  create    Create a git repo on SSB.',
+    ].join('\n'))
+    process.exit(0)
 
-var m = url.match(/^ssb:(?:\/\/)?(.*)$/)
-var root = m && m[1]
-if (!ref.isMsgId(root))
-    throw new Error(root || 'URL', 'is not a valid SSB message ID')
+  default:
+    console.error([
+      'Usage: git ssb [command]',
+      '',
+      'No such command "' + args[0] + '"'
+    ].join('\n'))
+    process.exit(1)
+}
 
-require('./client')(function (err, sbot) {
-  if (err) throw err
-  ssbGit.getRepo(sbot, root, function (err, repo) {
-    if (err) {
-      if (err.name == 'NotFoundError')
-        throw new Error('Repo not found with ID ' + root)
-      else
-        throw err
-    }
-
-    pull(
-      toPull(process.stdin),
-      require('pull-git-remote-helper')(repo),
-      toPull(process.stdout, function (err) {
-        sbot.close(err, function (err) {
-          if (err) throw err
-        })
-      })
-    )
+function createRepo(remoteName) {
+  require('./client')(function (err, sbot) {
+    if (err) throw err
+    ssbGit.createRepo(sbot, function (err, repo) {
+      if (err) throw err
+      var url = 'ssb://' + repo.id
+      console.log(url)
+      repo.close()
+      sbot.close()
+      if (remoteName)
+        spawn('git', ['remote', 'add', remoteName, url], {stdio: 'inherit'})
+    })
   })
-})
+}

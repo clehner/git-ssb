a9b72303067dfe180cdc0931c8297bcfad4ad5c1
cel <cel@f/6sQ6d2CMxRUhLpspgGIulDxDCwYD7DzFzPNr7u5AU=.ed25519>
2016-11-21 15:54:24 -0500

    Put usage strings into text files

--- bin.js
+++ bin.js
@@ -2,63 +2,17 @@
 ':' //; exec "$(command -v node || command -v nodejs)" "$0" "$@"
 // http://unix.stackexchange.com/questions/65235/universal-node-js-shebang
 
-try {
-  main()
-} catch(e) {
-  console.error(e instanceof Error ? e.stack : e)
-  process.exit(1)
-}
+if (/\/git-remote-ssb$/.test(process.argv[1])) {
+  require('git-remote-ssb/git-remote-ssb')
 
-function main() {
-  var path = require('path')
-  switch (path.basename(process.argv[1])) {
-    case 'git-remote-ssb':
-      return require('git-remote-ssb/git-remote-ssb')
+} else {
+  var u = require('./lib/util')
+  var config = require('ssb-config/inject')(u.getAppName())
+  var fn = require('./lib')
+  try {
+    fn(config)
+  } catch (e) {
+    console.error(e instanceof Error ? e.stack : e)
+    process.exit(1)
   }
-
-  var config = require('ssb-config/inject')(getAppName())
-  var cmd = config._.shift()
-  if (config.help)
-    return require('./lib/help')(cmd)
-  if (config.version)
-    return version()
-
-  switch (cmd) {
-    case 'create':
-      return require('./lib/create')(config)
-    case 'fork':
-      return require('./lib/fork')(config)
-    case 'forks':
-      return require('./lib/forks')(config)
-    case 'issues':
-      return require('./lib/issues')(config)
-    case 'prs':
-    case 'pull-requests':
-      return require('./lib/pull-requests')(config)
-    case 'name':
-      return require('./lib/name')(config)
-    case 'pull-request':
-      return require('./lib/pull-request')(config)
-    case 'web':
-      return require('git-ssb-web/server')
-    case 'help':
-      return require('./lib/help')(config._[0])
-    case 'version':
-      return version()
-    case undefined:
-      return require('./lib/help')()
-    default:
-      throw 'No such command \'' + cmd + '\''
-  }
-}
-
-function version() {
-  var pkg = require('./package')
-  console.log(pkg.name, pkg.version)
-}
-
-function getAppName() {
-  var proc = require('child_process')
-  return 'ssb_appname' in process.env ? process.env.ssb_appname :
-    proc.spawnSync('git', ['config', 'ssb.appname']).stdout.toString().trim()
 }
